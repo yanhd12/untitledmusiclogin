d5fcb45c2b7ce5cd677891031afadc50abde656b
yanhd12 <93908364+yanhd12@users.noreply.github.com>
2025-05-06 19:07:08 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const overlay = document.getElementById('overlay');
+const loginModal = document.getElementById('loginModal');
+const registerModal = document.getElementById('registerModal');
+
+function showLogin() {
+  overlay.classList.add('show');
+  loginModal.classList.add('show');
+  registerModal.classList.remove('show');
+}
+
+function showRegister() {
+  overlay.classList.add('show');
+  registerModal.classList.add('show');
+  loginModal.classList.remove('show');
+}
+
+function hideModals() {
+  overlay.classList.remove('show');
+  loginModal.classList.remove('show');
+  registerModal.classList.remove('show');
+}
